1b89089cbe061205305c219d34c95ad5304630e5
JERRY <hwjameshw@gmail.com>
2023-07-13 11:19:34 +0900

    feat: page

--- index.js
+++ index.js
@@ -2,17 +2,50 @@ import express from "express";
 
 import AdminJS from "adminjs";
 import AdminJSExpress from "@adminjs/express";
+import { ComponentLoader } from "adminjs";
 
 import { Resource, Database } from "@adminjs/prisma";
 import { PrismaClient } from "@prisma/client";
 
-import { initEnv } from "#util";
+import { v4 as uuid } from "uuid";
+import { toBinaryUUID, fromBinaryUUID } from "binary-uuid";
 
-import { User } from "./user.js";
+import { initEnv } from "#util";
 
 const env = initEnv();
 
 const prisma = new PrismaClient();
+
+prisma.$use(async (params, next) => {
+  console.log("HOOK PARAMS", params);
+  if (params.action === "create") {
+    if (params.args.data.uuid === undefined) {
+      const bin = toBinaryUUID(uuid());
+      params.args.data.uuid = bin;
+    }
+    if (!Buffer.isBuffer(params.args.data.uuid)) {
+      const bin = toBinaryUUID(uuid());
+      params.args.data.uuid = bin;
+    }
+  }
+  const result = await next(params);
+
+  if (Array.isArray(result)) {
+    return result.map((row) => {
+      if (row.uuid) {
+        const uuid = fromBinaryUUID(row.uuid);
+        row.uuid = uuid.toString();
+      }
+      return row;
+    });
+  } else {
+    if (result.uuid) {
+      const uuid = fromBinaryUUID(result.uuid);
+      result.uuid = uuid.toString();
+    }
+    return result;
+  }
+});
 
 AdminJS.registerAdapter({
   Resource: Resource,
@@ -23,23 +56,163 @@ const app = express();
 
 const dmmf = prisma._baseDmmf;
 
+const componentLoader = new ComponentLoader();
+
+const Components = {
+  Dashboard: componentLoader.add("Dashboard", "./dashboard"),
+};
+
+const usersProperties = [
+  "id",
+  "uuid",
+  "email",
+  "nickname",
+  "mbti",
+  "notification",
+  "active",
+  "created_at",
+];
+
+const postsProperties = [
+  "id",
+  "user_id",
+  "uuid",
+  "mbti",
+  "title",
+  "content",
+  "like_count",
+  "comment_count",
+  "active",
+  "created_at",
+];
+
+const commentsProperties = [
+  "id",
+  "post_id",
+  "user_id",
+  "uuid",
+  "content",
+  "active",
+  "created_at",
+];
+
 const admin = new AdminJS({
   rootPath: "/",
   loginPath: "/login",
   logoutPath: "logout",
+  componentLoader,
   branding: {
     companyName: "OhsunDosun",
+    logo: "/asset/logo.png",
+    withMadeWithLove: false,
+  },
+  locale: {
+    language: "en",
+    availableLanguages: ["en"],
+    localeDetection: true,
+    translations: {
+      en: {
+        components: {
+          Login: {
+            welcomeHeader: "Welcome",
+            welcomeMessage: "OhsunDosun Admin",
+          },
+        },
+      },
+    },
+  },
+  dashboard: {
+    component: Components.Dashboard,
   },
   resources: [
-    User,
     {
       resource: { model: dmmf.modelMap.users, client: prisma },
-      options: {},
+      options: {
+        navigation: true,
+        listProperties: usersProperties,
+        filterProperties: usersProperties,
+        sort: {
+          sortBy: "id",
+          direction: "desc",
+        },
+      },
+    },
+    {
+      resource: { model: dmmf.modelMap.posts, client: prisma },
+      options: {
+        navigation: true,
+        listProperties: postsProperties,
+        filterProperties: postsProperties,
+        sort: {
+          sortBy: "id",
+          direction: "desc",
+        },
+      },
+    },
+    {
+      resource: { model: dmmf.modelMap.comments, client: prisma },
+      options: {
+        navigation: true,
+        listProperties: commentsProperties,
+        filterProperties: commentsProperties,
+        sort: {
+          sortBy: "id",
+          direction: "desc",
+        },
+      },
+    },
+    {
+      resource: { model: dmmf.modelMap.user_ratings, client: prisma },
+      options: {
+        navigation: true,
+        sort: {
+          sortBy: "id",
+          direction: "desc",
+        },
+      },
+    },
+    {
+      resource: { model: dmmf.modelMap.reports, client: prisma },
+      options: {
+        navigation: true,
+        sort: {
+          sortBy: "id",
+          direction: "desc",
+        },
+      },
     },
   ],
 });
 
-const adminRouter = AdminJSExpress.buildRouter(admin);
+const DEFAULT_ADMIN = {
+  email: "admin@example.com",
+  password: "password",
+};
+
+const authenticate = async (email, password) => {
+  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
+    return Promise.resolve(DEFAULT_ADMIN);
+  }
+  return null;
+};
+
+const secret = "very_secret_secret";
+
+const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
+  admin,
+  {
+    authenticate,
+    cookiePassword: "very_secret_secret",
+  },
+  null,
+  {
+    resave: true,
+    saveUninitialized: true,
+    secret,
+  }
+);
+
+app.use(express.static("public"));
 
 app.use(admin.options.rootPath, adminRouter);
 
